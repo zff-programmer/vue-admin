db587ac484d328601e09fb7bcd71ddbdc39c4028
ronnie <zclzone@163.com>
2021-04-23 11:50:37 +0800

    项目构建、配置全局样式、路由、elementui

--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,18 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
+
   {
     path: '/',
-    name: 'Home',
-    component: Home
+    redirect: 'login'
+  },
+  {
+    path: '/login',
+    name: 'Login',
+    component: () => import('../views/Login/index.vue')
   },
   {
     path: '/about',
@@ -17,7 +21,7 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+  },
 ]
 
 const router = new VueRouter({
---
+++ src/views/Login/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="login">
+    登录页
+    <el-button>默认按钮</el-button>
+    <button>测试按钮</button>
+  </div>
+</template>
+<script>
+export default {
+  setup() {},
+}
+</script>
+<style scoped>
+#login {
+  background-color: #334960;
+  height: 100vh;
+}
+</style>
